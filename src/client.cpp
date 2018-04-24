e727b43a7b5e274c17fa19c3ed7b9f371ba54bb0
Unknown <pedromiguel.fernandes17@gmail.com>
2018-04-24 15:54:54 +0100

    added argument parser

--- src/client.cpp
+++ src/client.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include <sstream>
 #include "utils.h"
+#include <vector>
 
 using namespace std;
 
@@ -9,12 +11,42 @@ int usage(char *argv[])
     return 1;
 }
 
+vector<int> parseList(string list)
+{
+    istringstream str(list);
+    vector<int> parsed;
+    int value;
+
+    while (str >> value)
+        parsed.push_back(value);
+
+    return parsed;
+}
+
+int parseArguments(char *argv[], int &time_out, int &num_wanted_seats, vector<int> &pref_seat_list)
+{
+    if (!(isNumber(argv[1]) && isNumber(argv[2])))
+        return 1;
+
+    time_out = stoi(argv[1]);
+    num_wanted_seats = stoi(argv[2]);
+    pref_seat_list = parseList(argv[3]);
+
+    if (pref_seat_list.size() == 0)
+        return 1;
+
+    return 0;
+}
+
 int main(int argc, char *argv[])
 {
     if (argc != 4)
         return usage(argv);
 
-    if (!(isNumber(argv[1]) && isNumber(argv[2])))
+    int time_out, num_wanted_seats;
+    vector<int> prefList;
+
+    if (parseArguments(argv, time_out, num_wanted_seats, prefList) == 1)
         return invalidArguments();
 
     return 0;
--- src/server.cpp
+++ src/server.cpp
@@ -9,12 +9,26 @@ int usage(char *argv[])
     return 1;
 }
 
+int parseArguments(char *argv[], int &num_room_seats, int &num_ticket_offices, int &open_time)
+{
+    if (!(isNumber(argv[1]) && isNumber(argv[2]) && isNumber(argv[3])))
+        return 1;
+
+    num_room_seats = stoi(argv[1]);
+    num_ticket_offices = stoi(argv[2]);
+    open_time = stoi(argv[3]);
+
+    return 0;
+}
+
 int main(int argc, char *argv[])
 {
     if (argc != 4)
         return usage(argv);
 
-    if (!(isNumber(argv[1]) && isNumber(argv[2]) && isNumber(argv[3])))
+    int num_room_seats, num_ticket_offices, open_time;
+
+    if (parseArguments(argv, num_room_seats, num_ticket_offices, open_time) == 1)
         return invalidArguments();
 
     return 0;
